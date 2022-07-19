8bb79846f257f5fb7f5c25d87ed8776e77f0cb58
asanchez <andreucas1997@gmail.com>
2022-07-19 10:37:29 +0200

    give a timeout to make work the form

--- views/js/paylands.js
+++ views/js/paylands.js
@@ -80,15 +80,13 @@ $(document).ready(function (){
 	/**
 	 * Load paylands form
 	 */
-	if ($('input[name="payment-option"]').is(':checked') ){
-		if($('input[name="module-name"]') === "paylands") {
-			paylandsModel.initForm();
-		}
+	if($('input[name="payment-option"]:checked').data("module-name") === 'paylands'){
+		setTimeout(paylandsModel.initForm, 3000);
 	} else {
 		$(document).on("click",'input[name="payment-option"]', function() {
 			let input = $(this);
 			if(input.data("module-name") === "paylands") {
-				paylandsModel.initForm();
+				setTimeout(paylandsModel.initForm, 3000);
 			}
 		});
 	}
